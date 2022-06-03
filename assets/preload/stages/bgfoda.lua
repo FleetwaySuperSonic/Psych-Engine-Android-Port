8602bfd2f0c4ff2c58e830c449a7ad7c422b9392
FleetwaySuperSonic <95626256+FleetwaySuperSonic@users.noreply.github.com>
2022-06-03 12:24:13 -0300

    Add files via upload

---
+++ assets/preload/stages/bgfoda.lua
@@ -0,0 +1,10 @@
+function onCreate()
+    makeLuaSprite('rua2', 'rua2', 600, -70)
+    addLuaSprite('rua2', false)
+    
+    makeLuaSprite('rua1', 'rua1', 0, -70)
+    addLuaSprite('rua1', false)
+    
+    makeLuaSprite('bg', 'bg', 755, -90)
+    addLuaSprite('bg', false)
+end
